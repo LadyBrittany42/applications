b64f4e928d2d7dc2407e368a5e075ac12e46c816
LadyBrittany42 <brittgreen1983@gmail.com>
2022-04-26 14:22:11 +0000

    today

--- ifsc_3300/futurevaluecalculator/assets/js/future_value.js
+++ ifsc_3300/futurevaluecalculator/assets/js/future_value.js
@@ -1,4 +1,68 @@
-let investment = 0;
+"use strict";
+
+const $ = selector => document.querySelector(selector);
+
+const calculateFV = (investment, rate, years) => {
+    let futureValue = investment;
+    for (let i = 1; i <= years; i++ ) {
+        futureValue += futureValue * rate / 100;
+    }
+    futureValue = futureValue.toFixed(2);
+    return futureValue;
+};
+
+// const processEntries = () => {
+//     const investment = parseFloat($("#investment").value);
+//     const rate = parseFloat($("#rate").value);
+//     const years = parseInt($("#years").value);
+//     let errorMessage = "";
+
+//     if (isNaN(investment) || investment <= 0 || investment > 100000) {
+//         errorMessage = "Investment must be a number greater than 0 and less than or equal to 100,000";
+//         $("#investment").focus();
+//     } else if (isNaN(rate) || rate <= 0 || rate > 15) {
+//         errorMessage = "Rate must be a number greater than 0 and less than or equal to 15";
+//         $("#rate").focus();
+//     } else if (isNaN(years) || years <= 0 || years > 50) {
+//         errorMessage = "Years must be a number greater than 0 and less than or equal to 50";
+//         $("#years").focus();
+//     }
+//     if (errorMessage == "") {
+//         $("#future_value").value = calculateFV(investment, rate, years);        
+//     } else {
+//         alert(errorMessage);
+//     }
+
+// }
+
+document.addEventListener("DOMContentLoaded", () => {
+    $("#calculate").addEventListener("click", () => {
+        const investment = parseFloat($("#investment").value);
+        const rate = parseFloat($("#rate").value);
+        const years = parseInt($("#years").value);
+        let errorMessage = "";
+    
+        if (isNaN(investment) || investment <= 0 || investment > 100000) {
+            errorMessage = "Investment must be a number greater than 0 and less than or equal to 100,000";
+            $("#investment").focus();
+        } else if (isNaN(rate) || rate <= 0 || rate > 15) {
+            errorMessage = "Rate must be a number greater than 0 and less than or equal to 15";
+            $("#rate").focus();
+        } else if (isNaN(years) || years <= 0 || years > 50) {
+            errorMessage = "Years must be a number greater than 0 and less than or equal to 50";
+            $("#years").focus();
+        }
+        if (errorMessage == "") {
+            $("#future_value").value = calculateFV(investment, rate, years);        
+        } else {
+            alert(errorMessage);
+        }
+    });
+    $("#investment").focus();
+});
+
+
+/*let investment = 0;
 do {investment = parseFloat( prompt("Enter investment amount as xxxxx.xx", 10000));
     }
     while (isNaN (investment));
@@ -20,4 +84,4 @@ do {investment = parseFloat( prompt("Enter investment amount as xxxxx.xx", 10000
         document.write('<p><label><Interest rate:</label> ${rate}</p>');
         document.write('<p><label>Years:</label> ${years}</p>');
         document.write('<p><label>Future Value:</label> ${futureValue.toFixed(2)}</p>');
-    
+    */
